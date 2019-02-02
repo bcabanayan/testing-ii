1e03a6f711e601fba92193945012146fce5f1656
Bruce Cabanayan <bcabanayan@Bruces-MacBook-Pro.local>
2019-02-02 10:01:18 -0700

    Imported components and dependencies into test file and wrote boilerplate for tests

--- baseball/src/App.test.js
+++ baseball/src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { render, fireEvent } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import Dashboard from './Dashboard.js';
+import Display from './Display.js';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('The Display component', () => {
+
+});
+
+describe('The Dashboard component', () => {
+
 });
